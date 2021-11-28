04cf6753a2ffac73e957467eb2af211dd32059e2
Phil <PhilboBaggins@users.noreply.github.com>
2021-11-28 23:00:47 +1100

    Print some interesting datetimes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use unix_ts_macros::ts;
+
+fn print_time(n: u32) {
+    let t = ts!(n);
+    println!("{:#08X} {}", n, t.to_utc_datetime());
+}
+
 fn main() {
-    println!("Hello, world!");
+    /*
+    print_time(0x00000000);
+    print_time(0x000F0000);
+    print_time(0x00100000);
+    print_time(0x00FF0000);
+    print_time(0x01000000);
+    print_time(0x0FFF0000);
+
+    print_time(0x10000000);
+    print_time(0x20000000);
+    print_time(0x30000000);
+    print_time(0x40000000);
+    print_time(0x50000000);
+    print_time(0x60000000);
+    print_time(0x70000000);
+    print_time(0x80000000);
+    print_time(0x90000000);
+    print_time(0xA0000000);
+    print_time(0xB0000000);
+    print_time(0xC0000000);
+    print_time(0xD0000000);
+    print_time(0xE0000000);
+    print_time(0xF0000000);
+    */
+
+    print_time(0x60606060);
+    print_time(0x61616161);
+    print_time(0x62626262);
+    print_time(0x63636363);
+    println!("");
+    
+    print_time(0x60000000);
+    print_time(0x66000000);
+    print_time(0x66600000);
+    print_time(0x66660000);
+    print_time(0x66666666);
 }
